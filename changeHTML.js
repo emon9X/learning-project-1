8c0b0b090c851531dbc3f43761730cb3aa909263
emon9X <shahariar.emn990@gmail.com>
2023-12-14 12:37:05 +0600

    more than one function can be declared in a js file which can be used/called in an  HTML document

--- changeHTML.js
+++ changeHTML.js
@@ -1,3 +1,9 @@
 function changeHTML() {
     document.getElementById("change").innerHTML='I have changed on Click';
 }
+
+function changeColor() {
+    document.getElementById('text').style.color='red';
+}
+
+// more than one function can be declared in a js file 
