0c67dc6f76dd61f972226748d8210a7bd5f1798e
emon9X <shahariar.emn990@gmail.com>
2023-12-19 11:41:17 +0600

    the previous js output are shown via function which was unnecessary

--- changeHTML.js
+++ changeHTML.js
@@ -6,7 +6,5 @@ function changeColor() {
     document.getElementById('text').style.color='red';
 }
 
-function javaScriptOutput(x) {
-    x
-}
+
 // more than one function can be declared in a js file 
